1d7e78e4e9fa054ebe6d9a8ad78e46eab37bbeb9
Alexander Friedrich <johnendzsocial@icloud.com>
2020-01-24 12:28:50 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+// mudar imagem da tela inicial | change image home
+document.querySelector("._6b6a").src="https://i.imgur.com/kVoQ7nW.jpg";
+window.onload = function() { document.querySelector("._6b6a").style.display = "block"; }
