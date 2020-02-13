acb8f6967849f969b62395a474556fc6296cb8a2
Alexander Friedrich <johnendzsocial@icloud.com>
2020-02-13 20:16:04 -0300

    Update version 1.6

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // mudar imagem da tela inicial | change image home
 setTimeout(function() {
-    document.querySelector("._6b6a._3ftg").src="https://raw.githubusercontent.com/johnendz/Firefox-Extension-Messenger-Dark/master/kVoQ7nW.png";
+    document.querySelector("._6b6a._3ftg").src="https://raw.githubusercontent.com/johnendz/Firefox-Extension-Messenger-Dark/master/kVoQ7nW.jpg";
 }, 1000);
 setTimeout(function() {
     document.querySelector("._6b6a._3ftg").style.display = "block";
