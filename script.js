eaaeeb5f1f190787bcb1ce2b041f52260e4cd13d
Alexander Friedrich <johnendzsocial@icloud.com>
2020-03-15 11:31:29 -0300

    Update version 2.0

--- script.js
+++
@@ -1,7 +0,0 @@
-// mudar imagem da tela inicial | change image home
-setTimeout(function() {
-    document.querySelector("._6b6a._3ftg").src="https://i.imgur.com/0ey3ODr.jpg";
-}, 1000);
-setTimeout(function() {
-    document.querySelector("._6b6a._3ftg").style.display = "block";
-}, 2000);
